06243d59e4a3df6e9618037c2f652dcaaacc20c6
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-11 07:58:21 +0200

    Create products routes and static controller for it

---
+++ controllers/productController.js
@@ -0,0 +1,32 @@
+const createProduct = async (req, res) => {
+    res.send("ok");
+};
+
+const getAllProducts = async (req, res) => {
+    res.send("ok");
+};
+
+const getSingleProduct = async (req, res) => {
+    res.send("ok");
+};
+
+const updateProduct = async (req, res) => {
+    res.send("ok");
+};
+
+const deleteProduct = async (req, res) => {
+    res.send("ok");
+};
+
+const uploadImage = async (req, res) => {
+    res.send("ok");
+};
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    uploadImage,
+    deleteProduct
+};
---
+++ routes/productRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+} = require('../controllers/productController');
+
+router
+  .route('/')
+  .post(createProduct)
+  .get(getAllProducts);
+
+router
+  .route('/uploadImage')
+  .post(uploadImage);
+
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch( updateProduct)
+  .delete( deleteProduct);
+
+module.exports = router;
