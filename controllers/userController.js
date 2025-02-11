0cd97431329fd514218984897e258e6eee152744
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-11 08:14:16 +0200

    Creating user routes and static controller for it

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const getAllUsers = async (req, res) => {
+    res.send("ok");
+};
+
+const getSingleUser = async (req, res) => {
+    res.send("ok");
+};
+
+const showCurrentUser = async (req, res) => {
+    res.send("ok");
+};
+
+const updateUser = async (req, res) => {
+    res.send("ok");
+};
+
+const updateUserPassword = async (req, res) => {
+    res.send("ok");
+};
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+};
