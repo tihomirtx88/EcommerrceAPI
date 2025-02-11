ffef982a3fd690241a969bd7a5ec63a082e51e31
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-11 09:09:54 +0200

    Update functionality in auth controller in case register user for user role

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,18 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("./../errors");
 
 const register = async (req, res) => {
-    const { email } = req.body;
+    const { email, name, password } = req.body;
 
     const emailAlreadyExist = await User.findOne({email});
 
     if (emailAlreadyExist) {
         throw new CustomError.BadRequestError('Email alredy exists!');
     }
-    const user = await User.create(req.body);
+
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : 'user';
+
+    const user = await User.create({name, email, password, role});
     res.status(StatusCodes.CREATED).json({user})
 };
 
