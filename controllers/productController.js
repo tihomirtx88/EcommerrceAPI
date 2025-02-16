6670d64bb14d31fa67ceefdabc5d4ffc6f3d59e0
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-16 11:52:59 +0200

    Implement get single product functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,39 +1,48 @@
-const Product = require('./../models/Product');
+const Product = require("./../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("./../errors");
 
 const createProduct = async (req, res) => {
-    req.body.user = req.user.userId;
-    const porduct = await Product.create(req.body);
-    res.status(StatusCodes.CREATED).json({porduct});
+  req.body.user = req.user.userId;
+  const porduct = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ porduct });
 };
 
 const getAllProducts = async (req, res) => {
-    const porducts = await Product.find({});
-    res.status(StatusCodes.OK).json({porducts, count: porducts.length});
+  const porducts = await Product.find({});
+  res.status(StatusCodes.OK).json({ porducts, count: porducts.length });
 };
 
 const getSingleProduct = async (req, res) => {
-    res.send("ok");
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(
+      `There is no product with id: ${productId}`
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const updateProduct = async (req, res) => {
-    res.send("ok");
+  res.send("ok");
 };
 
 const deleteProduct = async (req, res) => {
-    res.send("ok");
+  res.send("ok");
 };
 
 const uploadImage = async (req, res) => {
-    res.send("ok");
+  res.send("ok");
 };
 
 module.exports = {
-    createProduct,
-    getAllProducts,
-    getSingleProduct,
-    updateProduct,
-    uploadImage,
-    deleteProduct
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  uploadImage,
+  deleteProduct,
 };
