f75fad979743e2862dcd63b0020eac1d77168d0a
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-16 10:59:44 +0200

    Implemenet checkPermision function to avoid case current user to see information about other users

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const { isTokeValid, createJWT, atachCookieToResponse } = require('./jwt');
 const { createTokenUser } = require("./../utils/createTokeUser");
+const { checkPermissions } = require("./../utils/checkPermissions");
 
 module.exports = {
     isTokeValid, 
     createJWT,
     atachCookieToResponse,
-    createTokenUser
+    createTokenUser,
+    checkPermissions
 };
