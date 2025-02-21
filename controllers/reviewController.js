86a4078b3968c6411a4810324c4b73354f6f1946
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-21 19:44:38 +0200

    Implemenet deleting review functionality

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -51,7 +51,19 @@ const updateReview = async (req, res) => {
 };
 
 const deleteReview = async (req, res) => {
-  res.send("ok");
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review) {
+    throw new CustomError.NotFoundError(
+      `There is no review with id: ${reviewId}`
+    );
+  }
+
+  checkPermissions(req.user, review.user);
+  await review.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Review is removed' });
 };
 
 module.exports = {
