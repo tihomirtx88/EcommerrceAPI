a43354980866d6a5f1a5a0c130e84a496a78bd58
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-24 09:34:52 +0200

    Implemenet updated order functionality

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -98,7 +98,23 @@ const createOrder = async (req, res) => {
 };
 
 const updateOrder = async (req, res) => {
-  res.send("ok");
+  const { id: orderId } = req.params;
+  const { paymentIntentId } = req.body;
+  const order = await Order.findOne({ _id: orderId });
+
+  if (!order) {
+    throw new CustomError.NotFoundError(
+      `There is no order with id: ${orderId}`
+    );
+  }
+
+  checkPermissions(req.user, order.user);
+
+  order.paymentIntentId = paymentIntentId;
+  order.status = 'paid';
+  await order.save();
+
+  res.status(StatusCodes.OK).json({ order });
 };
 
 module.exports = {
