3e7da351a4a723aad963b160b8e735fbbb544134
tihomirtx88 <tihomirtx88@gmail.com>
2025-02-26 08:09:47 +0200

    Import and use extra security and support libraries

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const mongooseSanitize = require("express-mongo-sanitize");
 
 const connectDB = require("./db/db");
 // const authenticateUser = require('./middleware/authentication');
@@ -27,6 +29,12 @@ const orderRouter = require('./routes/orderRoutes');
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set('proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 60
+}));
+
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -35,6 +43,7 @@ app.use(fileUpload());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+app.use(mongooseSanitize());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
